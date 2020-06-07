e9a1f89f9009db940216aeda1c06558a7a2ea9c8
JacksonCampolattaro <jackcamp@vt.edu>
2020-06-07 00:01:40 -0400

    Add placeholder plot.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,24 @@
 #include <iostream>
 #include "gnuplot_i.hpp"
 
+
+
+float bench_old(std::size_t dataset_size) {}
+float bench_new(std::size_t dataset_size) {}
+
+using std::cin;
+using std::cout;
+using std::endl;
+
 int main() {
 
+  Gnuplot plot("lines");
+
+  plot.set_title("test");
+  plot.plot_slope(1.0,0.0,"y=x");
+
+  cout << endl << "Press ENTER to continue..." << endl;
+  std::cin.clear();
+  std::cin.ignore(std::cin.rdbuf()->in_avail());
+  std::cin.get();
 }
