f59a6fd1e2a5505cbf7717a88723668cf4bb29ed
JacksonCampolattaro <jackcamp@vt.edu>
2020-06-07 12:01:22 -0400

    Add timing code

--- main.cpp
+++ main.cpp
@@ -2,28 +2,45 @@
 // Created by jackcamp on 6/6/20.
 //
 
-#include <iostream>
 #include "gnuplot_i.hpp"
-
-
-
-double bench_old(std::size_t dataset_size) {
-  return dataset_size;
-}
-
-double bench_new(std::size_t dataset_size) {
-  return dataset_size * 2;
-}
+#include <iostream>
+#include <chrono>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
+using namespace std::chrono;
+
+size_t bench_old(std::size_t dataset_size) {
+
+  auto start = high_resolution_clock::now();
+
+  // Code to be timed
+
+  auto end = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(end - start).count();
+
+  return duration;
+}
+
+size_t bench_new(std::size_t dataset_size) {
+
+  auto start = high_resolution_clock::now();
+
+  // Code to be timed
+
+  auto end = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(end - start).count();
+
+  return duration;
+}
+
 int main() {
 
-  std::vector<double> x, yOld, yNew;
+  std::vector<size_t> x, yOld, yNew;
 
-  for (std::size_t i = 0; i < 10000; ++i) {
+  for (size_t i = 0; i < 10000; ++i) {
 
     x.push_back(i);
     yOld.push_back(bench_old(i));
@@ -32,14 +49,13 @@ int main() {
 
 
   Gnuplot plot("lines");
-
   plot.set_title("test");
   plot.plot_xy(x, yOld, "Old");
   plot.plot_xy(x, yNew, "New");
 
 
   cout << endl << "Press ENTER to continue..." << endl;
-  std::cin.clear();
-  std::cin.ignore(std::cin.rdbuf()->in_avail());
-  std::cin.get();
+  cin.clear();
+  cin.ignore(cin.rdbuf()->in_avail());
+  cin.get();
 }
