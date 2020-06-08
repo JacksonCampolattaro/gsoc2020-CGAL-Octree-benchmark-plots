7b00ef09bf759e543fd6b7ed6b5cad21b1411867
JacksonCampolattaro <jackcamp@vt.edu>
2020-06-08 12:45:47 -0400

    Move benchmarking functions to new header

---
+++ benchmark.h
@@ -0,0 +1,91 @@
+//
+// Created by jackcamp on 6/8/20.
+//
+
+#ifndef BENCHMARK_BENCHMARK_H
+#define BENCHMARK_BENCHMARK_H
+
+#include <chrono>
+
+#include "Octree.h"
+#include <CGAL/Shape_detection/Efficient_RANSAC/Efficient_RANSAC_traits.h>
+#include "Octree_3.h"
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Point_set_3.h>
+#include <CGAL/point_generators_3.h>
+
+
+#define SAMPLES_PER_TEST 10
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using namespace std::chrono;
+
+// Defining the kernel
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::Point_3 Point;
+typedef CGAL::Point_set_3<Point> Point_set;
+typedef CGAL::Point_set_3<Point> Point_set;
+
+// Defining the Old Octree
+typedef CGAL::Shape_detection::Efficient_RANSAC_traits
+        <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
+        Traits;
+typedef CGAL::Shape_detection::internal::Octree
+        <CGAL::Shape_detection::internal::DirectPointAccessor<Traits>>
+        OldOctree;
+
+// Defining the New Octree
+typedef CGAL::OCTREE::Octree
+        <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
+        NewOctree;
+
+static const int repetitions = 100;
+
+int bench_old(Point_set points) {
+
+  auto point_map = points.point_map();
+  auto normal_map = points.normal_map();
+  auto input_iterator_first = points.begin();
+  auto input_iterator_beyond = points.end();
+
+  int duration = 0;
+  for (int i = 0; i < repetitions; ++i) {
+
+    auto start = high_resolution_clock::now();
+
+    auto oldOctree = OldOctree((Traits()), input_iterator_first,
+                               input_iterator_beyond, point_map, normal_map);
+    oldOctree.createTree();
+
+    auto end = high_resolution_clock::now();
+    duration += duration_cast<microseconds>(end - start).count();
+  }
+
+  return duration / repetitions;
+}
+
+int bench_new(Point_set points) {
+
+  auto point_map = points.point_map();
+  auto normal_map = points.normal_map();
+  auto input_iterator_first = points.begin();
+  auto input_iterator_beyond = points.end();
+
+  int duration = 0;
+  for (int i = 0; i < repetitions; ++i) {
+
+    auto start = high_resolution_clock::now();
+
+    NewOctree newOctree(points, point_map, normal_map);
+
+    auto end = high_resolution_clock::now();
+    duration += duration_cast<microseconds>(end - start).count();
+  }
+
+  return duration / repetitions;
+}
+
+#endif //BENCHMARK_BENCHMARK_H
--- main.cpp
+++ main.cpp
@@ -6,91 +6,15 @@
 
 #include "gnuplot_i.hpp"
 #include <iostream>
-#include <chrono>
 
-#include "Octree.h"
-#include <CGAL/Shape_detection/Efficient_RANSAC/Efficient_RANSAC_traits.h>
-#include "Octree_3.h"
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Point_set_3.h>
-#include <CGAL/point_generators_3.h>
+#include "benchmark.h"
 
-using std::cin;
-using std::cout;
-using std::endl;
-
-using namespace std::chrono;
-
-// Defining the kernel
-typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
-typedef Kernel::Point_3 Point;
-typedef CGAL::Point_set_3<Point> Point_set;
-typedef CGAL::Point_set_3<Point> Point_set;
-
-// Defining the Old Octree
-typedef CGAL::Shape_detection::Efficient_RANSAC_traits
-        <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
-        Traits;
-typedef CGAL::Shape_detection::internal::Octree
-<CGAL::Shape_detection::internal::DirectPointAccessor<Traits>>
-        OldOctree;
-
-// Defining the New Octree
-typedef CGAL::OCTREE::Octree
-        <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
-        NewOctree;
-
-static const int repetitions = 10;
-
-int bench_old(Point_set points) {
-
-  auto point_map = points.point_map();
-  auto normal_map = points.normal_map();
-  auto input_iterator_first = points.begin();
-  auto input_iterator_beyond = points.end();
-
-  int duration = 0;
-  for (int i = 0; i < repetitions; ++i) {
-
-    auto start = high_resolution_clock::now();
-
-    auto oldOctree = OldOctree((Traits()), input_iterator_first,
-                               input_iterator_beyond, point_map, normal_map);
-    oldOctree.createTree();
-
-    auto end = high_resolution_clock::now();
-    duration += duration_cast<microseconds>(end - start).count();
-  }
-
-  return duration / repetitions;
-}
-
-int bench_new(Point_set points) {
-
-  auto point_map = points.point_map();
-  auto normal_map = points.normal_map();
-  auto input_iterator_first = points.begin();
-  auto input_iterator_beyond = points.end();
-
-  int duration = 0;
-  for (int i = 0; i < repetitions; ++i) {
-
-    auto start = high_resolution_clock::now();
-
-    NewOctree newOctree(points, point_map, normal_map);
-
-    auto end = high_resolution_clock::now();
-    duration += duration_cast<microseconds>(end - start).count();
-  }
-
-  return duration / repetitions;
-}
 
 int main() {
 
   std::vector<int> x, yOld, yNew;
 
-  for (int N = 1; N < 1000; N += 1 + (N / 100)) {
+  for (int N = 1; N < 2000; N += 1 + (N / 1000)) {
 
     cout << N << endl;
 
@@ -108,6 +32,7 @@ int main() {
 
 
   Gnuplot plot("lines");
+  //plot.set_style("fill solid 1.0 noborder");
   plot.set_title("Comparison of Old and New Algorithms for Constructing an Octree");
   plot.set_ylabel("Time to Build a Tree (Microseconds)");
   plot.set_xlabel("Number of Points Added");
