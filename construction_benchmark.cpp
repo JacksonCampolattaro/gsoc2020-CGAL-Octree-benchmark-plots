7944eee9c4b87012759becd32ce98dfb68058778
Jackson Campolattaro <jackcamp@vt.edu>
2020-07-28 12:43:44 -0400

    Move construction benchmarks out of benchmark.h

--- construction_benchmark.cpp
+++ construction_benchmark.cpp
@@ -4,17 +4,113 @@
 
 #define CGAL_TRACE_STREAM std::cerr
 
-#include <CGAL/point_generators_3.h>
-#include <CGAL/Point_set_3/IO.h>
-#include <CGAL/random_simplify_point_set.h>
-
 #include "plotting.h"
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Point_set_3.h>
+#include <CGAL/Point_set_3/IO.h>
+#include <CGAL/point_generators_3.h>
+
+#include <CGAL/Octree.h>
+#include <CGAL/Octree/IO.h>
+
+#include "Octree.h"
+#include <CGAL/Shape_detection/Efficient_RANSAC/Efficient_RANSAC_traits.h>
+#include "Octree_3.h"
+
+#include <chrono>
 #include <iostream>
 #include <istream>
+#include <CGAL/random_simplify_point_set.h>
 
-#include "benchmark.h"
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::FT FT;
+typedef Kernel::Point_3 Point;
+typedef CGAL::Point_set_3<Point> Point_set;
+
+// Defining the Old Octree
+typedef CGAL::Shape_detection::Efficient_RANSAC_traits
+        <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
+        Traits;
+typedef CGAL::Shape_detection::internal::Octree
+        <CGAL::Shape_detection::internal::DirectPointAccessor<Traits>>
+        OldOctree;
+
+// Defining the New Octree
+typedef CGAL::OCTREE::Octree
+        <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
+        NewOctree;
+
+// Defining the Improved Octree
+typedef CGAL::Octree::Octree<Point_set, typename Point_set::Point_map> ImprovedOctree;
+typedef ImprovedOctree::FT FT;
+
+// Defining the kD tree
+typedef CGAL::Search_traits_3<Kernel> Kd_tree_traits;
+typedef CGAL::Orthogonal_k_neighbor_search<Kd_tree_traits> Kd_tree_search;
+typedef Kd_tree_search::Tree Kd_tree;
+
+typedef std::chrono::microseconds time_unit;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+
+using std::cout;
+using std::endl;
 
 #define SAMPLES_PER_TEST 10
+#define BUCKET_SIZE 20
+#define MAX_DEPTH 10
+
+int bench_old(Point_set points) {
+
+  auto point_map = points.point_map();
+  auto normal_map = points.normal_map();
+  auto input_iterator_first = points.begin();
+  auto input_iterator_beyond = points.end();
+
+  auto start = high_resolution_clock::now();
+
+  auto oldOctree = OldOctree((Traits()), input_iterator_first,
+                             input_iterator_beyond, point_map, normal_map,
+                             0, BUCKET_SIZE, MAX_DEPTH);
+
+  oldOctree.createTree();
+
+  auto end = high_resolution_clock::now();
+  return duration_cast<time_unit>(end - start).count();
+}
+
+int bench_new(Point_set points) {
+
+  auto point_map = points.point_map();
+  auto normal_map = points.normal_map();
+  auto input_iterator_first = points.begin();
+  auto input_iterator_beyond = points.end();
+
+  auto start = high_resolution_clock::now();
+
+  NewOctree newOctree(points, point_map, normal_map);
+  newOctree.refine(MAX_DEPTH, BUCKET_SIZE);
+
+  auto end = high_resolution_clock::now();
+  return duration_cast<time_unit>(end - start).count();
+}
+
+int bench_improved(Point_set points) {
+
+  auto point_map = points.point_map();
+  auto normal_map = points.normal_map();
+  auto input_iterator_first = points.begin();
+  auto input_iterator_beyond = points.end();
+
+  auto start = high_resolution_clock::now();
+
+  ImprovedOctree improvedOctree(points, point_map);
+  improvedOctree.refine(MAX_DEPTH, BUCKET_SIZE);
+
+  auto end = high_resolution_clock::now();
+  return duration_cast<time_unit>(end - start).count();
+}
 
 void synthetic_bench(int max_points, std::string file_path) {
 
--- search_benchmark.cpp
+++ search_benchmark.cpp
@@ -7,6 +7,7 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Point_set_3.h>
 #include <CGAL/point_generators_3.h>
+
 #include <CGAL/Octree.h>
 #include <CGAL/Octree/IO.h>
 
